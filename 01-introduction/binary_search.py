bd69f788b4d825cca45d614e768f7bbe784c90a9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-12 19:51:38 +0100

    chore: create the first implementation of `binary search` algorithm

---
+++ 01-introduction/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(ranged_list, item):
+    low = 0
+    high = len(ranged_list) - 1
+
+    while low <= high:
+        medium = (low + high) / 2
+        attempt = ranged_list[medium]
+
+        if attempt == item:
+            return medium
+        if attempt > item:
+            high = medium - 1
+        else:
+            low = medium + 1
+
+    return None
+
+example_list = [1, 3, 5, 7, 9]
+
+print(binary_search(example_list, 3))
+print(binary_search(example_list, -1))
